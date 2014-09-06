e9213a1d3e264da2aec56a84dd16f464cbb2080c
bitst0rm <bitst0rm@users.noreply.github.com>
2014-09-07 01:38:11 +0200

    Fixed E_NOTICE showing when product keyword is not set

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -110,7 +110,7 @@ class ModelCatalogProduct extends Model {
 			}
 		}
 
-		if ($data['keyword']) {
+		if (isset($data['keyword'])) {
 			$this->db->query("INSERT INTO " . DB_PREFIX . "url_alias SET query = 'product_id=" . (int)$product_id . "', keyword = '" . $this->db->escape($data['keyword']) . "'");
 		}
 
@@ -259,7 +259,7 @@ class ModelCatalogProduct extends Model {
 
 		$this->db->query("DELETE FROM " . DB_PREFIX . "url_alias WHERE query = 'product_id=" . (int)$product_id. "'");
 
-		if ($data['keyword']) {
+		if (isset($data['keyword'])) {
 			$this->db->query("INSERT INTO " . DB_PREFIX . "url_alias SET query = 'product_id=" . (int)$product_id . "', keyword = '" . $this->db->escape($data['keyword']) . "'");
 		}
 
