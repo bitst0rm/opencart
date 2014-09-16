4f732bb2b2112683e1a7acdef81623d81e41adc5
bitst0rm <bitst0rm@users.noreply.github.com>
2014-09-16 17:02:16 +0200

    Minor language corrections

--- upload/admin/language/english/sale/return.php
+++ upload/admin/language/english/sale/return.php
@@ -63,7 +63,7 @@ $_['error_order_id']      = 'Order ID required!';
 $_['error_firstname']     = 'First Name must be between 1 and 32 characters!';
 $_['error_lastname']      = 'Last Name must be between 1 and 32 characters!';
 $_['error_email']         = 'E-Mail Address does not appear to be valid!';
-$_['error_telephone']     = 'Telephone must be between 3 and 32 characters!';
+$_['error_telephone']     = 'Phone number must be between 3 and 32 digits!';
 $_['error_product']       = 'Product Name must be greater than 3 and less than 255 characters!';
 $_['error_model']         = 'Product Model must be greater than 3 and less than 64 characters!';
 ?>
